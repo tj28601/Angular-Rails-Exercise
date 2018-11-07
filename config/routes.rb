5835f735da89ee405a5f4e4aefd91d3e0841af48
Taylor Yount <tayloryount@Taylors-MacBook-Air.local>
2018-11-07 11:40:25 -0500

    successfully integrated with GitHub API

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  root 'homes#index'
+  root 'players#index'
   devise_for :users
+
+  resources :players
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :players 
+      resources :players
     end
   end
 end
