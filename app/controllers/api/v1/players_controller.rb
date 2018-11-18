b57b04fb63bbd25cec686fb1dba62e59c3ac205e
Taylor Yount <tayloryount@Taylors-MacBook-Air.local>
2018-11-17 19:17:13 -0500

    added create functionality to API controller

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -9,4 +9,18 @@ class Api::V1::PlayersController < ApplicationController
 
     # @players = Player.paginate(:page => params[:page], :per_page => 10)
   end
+# def new
+#   @player = Player.new
+# end
+  def create
+    @player = Player.new(player_params)
+    @player.save
+    # binding.pry
+  end
+
+  private
+
+  def player_params
+    params.require(:player).permit(:name)
+  end
 end
--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,4 +1,12 @@
 class PlayersController < ApplicationController
   def index
   end
+  def create
+  end
+
+  protected
+  
+  def player_params
+    params.require(:player).permit(:name)
+  end
 end
